6c30fdd37a23c07b32cf7a478504f67ace6c3c48
oliceglad <vycheslavofficial@gmail.com>
2024-04-13 17:00:03 +0400

    added lang data

---
+++ data.py
@@ -0,0 +1,17 @@
+data_lang = {
+    "sculpture": "Скульптура",
+    "books": "Книга",
+    "archaeology": "Археология",
+    "decorative-and-applied-arts": "ДПИ",
+    "documents": "Документы",
+    "graphics": "Графика",
+    "mineralogy": "Минералогия",
+    "natural-science-collection": "Естественно-научная коллекция",
+    "numismatics": "Нумизматика",
+    "other": "Прочее",
+    "painting": "Живопись",
+    "photographic-negatives": "Фото, негатив",
+    "printed-matter": "Печатная продукция",
+    "technology": "Техника",
+    "weapons": "Оружие"
+}
--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 import os
 import pandas as pd
 from ai.model import go_to_ai
+from data import data_lang
+
 def make_csv():
     static = os.listdir("static")
     columns = ['object_id', 'object_name', 'group']
     data = []
     for name in static:
         info = go_to_ai(name)
-        data.append(["", name, info["predictions"][0]["class"]])
+        data.append(["", name, data_lang[info["predictions"][0]["class"]]])
     df = pd.DataFrame(data, columns=columns)
     df.to_csv(r'trained.csv')
 
