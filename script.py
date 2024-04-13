cef88016d791231ab49c45ffde73e7e61f374d0c
oliceglad <vycheslavofficial@gmail.com>
2024-04-13 23:00:36 +0400

    added script which makes a csv file

--- script.py
+++ script.py
@@ -1,17 +1,24 @@
 import os
+import csv
 import pandas as pd
 from ai.model import go_to_ai
 from data import data_lang
 
 def make_csv():
     static = os.listdir("images")
-    columns = ['object_id', 'object_name', 'group']
+    columns = ['object_id', 'img_name', 'group']
     data = []
-    for name in static:
-        info = go_to_ai(name)
-        data.append(["", name, data_lang[info["predictions"][0]["class"]]])
+    with open('nontrain.csv', newline='', encoding="UTF-8") as f:
+        reader = list(csv.reader(f, delimiter=';', quoting=csv.QUOTE_MINIMAL))
+        for name in static:
+            id = ''
+            for row in reader[1:]:
+                if name in row:
+                    id = row[0]
+            info = go_to_ai(name)
+            data.append([id, name, data_lang[info["predictions"][0]["class"]]])
     df = pd.DataFrame(data, columns=columns)
-    df.to_csv(r'trained.csv')
+    df.to_csv(r'trained.csv', sep = ';', encoding = 'utf-8', index = False)
 
 if __name__ == "__main__":
     make_csv()
