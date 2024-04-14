071d6fd0de0a1ee06b0373c6fdcf2e4dc286c093
oliceglad <vycheslavofficial@gmail.com>
2024-04-14 11:07:34 +0400

    changed script

--- ai/model.py
+++ ai/model.py
@@ -8,14 +8,12 @@ load_dotenv()
 api = os.getenv("API", None)
 root_dir = os.path.dirname(os.path.abspath(__file__)).replace("/ai", "")
 folder = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "..", "static",
+    os.path.dirname(os.path.abspath(__file__)), "..", "images",
 )
 
 def go_to_ai(image) -> Dict:
     rf = Roboflow(api_key=api)
-    project = rf.workspace().project("museum-objects")
+    project = rf.workspace().project("museum-xvhrs")
     model = project.version(1).model
-    model.predict(f'{folder}/{image}', confidence=40, overlap=30).save(f'{folder}/new_{image}')
-    info = model.predict(f'{folder}/{image}', confidence=40, overlap=30).json()
-    os.remove(f'{folder}/{image}')
+    info = model.predict(f'{folder}/{image}').json()
     return info
