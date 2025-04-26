67ed1fad66b9554cda7855e0ee1edea0123c30a0
oliceglad <vycheslavofficial@gmail.com>
2025-04-26 23:35:38 +0800

    added s3.py

---
+++ app/s3.py
@@ -0,0 +1,36 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+from aiobotocore.session import get_session
+
+load_dotenv()
+
+class S3Client:
+    def __init__(self,
+                 access_key: str,
+                 secret_key: str,
+                 endpoint_url: str,
+                 bucket_name: str,):
+        self.config = {
+            "aws_access_key_id": access_key,
+            "aws_secret_access_key":secret_key,
+            "endpoint_url": endpoint_url,
+            "verify": False,
+        }
+        self.bucket_name = bucket_name
+        self.session = get_session()
+
+    @asynccontextmanager
+    async def get_client(self):
+        async with self.session.create_client("s3", **self.config) as client:
+            yield client
+
+    async def upload_file(self,file_path: str,object_name:str,):
+        async with self.get_client() as client:
+            with open(file_path, "rb") as file:
+                await client.put_object(
+                    Bucket=self.bucket_name,
+                    Key=object_name,
+                    Body=file
+                )
